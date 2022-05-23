fa4a44f6a4f2e2e74532b1890e34660a700e1a27
lucas sotero <lucas.15.sotero@gmail.com>
2022-05-23 12:07:15 -0300

    iniciando integração com FXML ao main do projeto livro interativo

--- src/App.java
+++ src/App.java
@@ -1,26 +1,43 @@
 import java.util.Map;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
-public class App {
+public class App extends Application {
 
  public static void main(String[] args )
   //Criação do novo arquivo
- {       
-     
+ {        
     
-    
- // Scanner escaneador = new Scanner(System.in, "CP850");
-  //LeitorDeArquivos leitor = new LeitorDeArquivos();
-  //Map<String, Personagem> personagens = leitor.lerPersonagens("rsc/Personagens.txt"); //dicionario de personagens, onde estão todos armazenados.
+    Scanner escaneador = new Scanner(System.in, "CP850");
+  LeitorDeArquivos leitor = new LeitorDeArquivos();
+  Map<String, Personagem> personagens = leitor.lerPersonagens("rsc/Personagens.txt"); //dicionario de personagens, onde estão todos armazenados.
 
-  //Map<String, Capitulo> capitulos = leitor.lerCapitulos("rsc/Capitulos.txt",personagens,escaneador); // dicionario de capitulos que é integrado com as escolhas e personagens quando for lido
+  Map<String, Capitulo> capitulos = leitor.lerCapitulos("rsc/Capitulos.txt",personagens,escaneador); // dicionario de capitulos que é integrado com as escolhas e personagens quando for lido
  
-  //Capitulo raiz = capitulos.get("Raiz"); //lemos o capitulo respectivo a raiz e mostramos
+  Capitulo raiz = capitulos.get("Raiz"); //lemos o capitulo respectivo a raiz e mostramos
   //raiz.play();
 
-  //escaneador.close();
+  escaneador.close();
+
+  launch(args);
+}
+  @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Parent root = loader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Livro Interativo !!");
+        primaryStage.setScene(tela);
+        primaryStage.show();
+    }
  }
   
-}
+
 
--- src/Controlador.java
+++ src/Controlador.java
@@ -1,28 +1,37 @@
-import javafx.event.ActionEvent;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.TextArea;
 
 public class Controlador {
 
     @FXML
-    private Button botaoiniciar;
+    private ButtonBar botoesEscolhas;
 
     @FXML
-    private Label labelNomePersonagem;
+    private TextArea imagemACII;
 
     @FXML
-    private TextField textFieldNomePersonagem;
+    private TextArea textoCapitulo;
 
-    @FXML
-    private Label titulo;
-
-    @FXML
-    void salvarNomePersonagem(ActionEvent event)
+    public void mostrarTextoCapitulo(String texto)
     {
-        String nome = textFieldNomePersonagem.getText();
-        labelNomePersonagem.setText(nome);
+        textoCapitulo.setText(texto);
     }
-
+    public void mostrarImagemCapitulo(String imagem)
+    {
+        imagemACII.setText(imagem);
+    }
+    public void mostrarEscolhas(ArrayList<Escolha> escolhas)
+    {
+        botoesEscolhas.setPadding(new Insets(10,10, 10, 10));
+        for (int i = 0; i < escolhas.size(); i++)
+        {
+            botoesEscolhas.getButtons().add(new Button(escolhas.get(i).getTextoMostrado()));
+            //System.out.println(escolhas.get(i).getTextoMostrado());
+        }
+    }
 }
